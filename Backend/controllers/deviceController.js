3a1e75b55bf4cb250446c31861bad71c9f7893d4
Shehan - karunarathne <87872258+shehan-karunarathna@users.noreply.github.com>
2024-04-07 22:05:25 +0530

    Add files via upload

---
+++ Backend/controllers/deviceController.js
@@ -0,0 +1,8 @@
+// deviceController.js
+const Device = require('../models/Device');
+
+exports.getAllDevices = async (req, res) => {  };
+exports.getDeviceById = async (req, res) => {  };
+exports.createDevice = async (req, res) => {  };
+exports.updateDevice = async (req, res) => {  };
+exports.deleteDevice = async (req, res) => {  };
---
+++ Backend/controllers/locationController.js
@@ -0,0 +1,10 @@
+// locationController.js
+const Location = require('../models/Location');
+
+exports.getAllLocations = async (req, res) => {  };
+exports.getLocationById = async (req, res) => {  };
+exports.createLocation = async (req, res) => {  };
+exports.updateLocation = async (req, res) => {  };
+exports.deleteLocation = async (req, res) => {  };
+exports.addDeviceToLocation = async (req, res) => { };
+exports.removeDeviceFromLocation = async (req, res) => {  };
